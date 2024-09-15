f3b3eadfeff0733cd5a795dc4919e8b792ce8042
PurrProof <149718167+PurrProof@users.noreply.github.com>
2024-09-15 14:45:58 +0200

    add GuessTheNumber level solution

--- test/02-nickname.ts
+++ test/02-nickname.ts
@@ -1,10 +1,9 @@
 import { SignerWithAddress as HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 import { expect } from "chai";
-import { BaseContract } from "ethers";
 import hre from "hardhat";
 
-import { NicknameChallenge } from "../typechain-types";
 import { CaptureTheEther, CaptureTheEther__factory } from "../typechain-types";
+import { NicknameChallenge, NicknameChallenge__factory } from "../typechain-types";
 
 describe("2. Warmup -> Choose a nickname", function () {
   let owner: HardhatEthersSigner;
@@ -21,11 +20,8 @@ describe("2. Warmup -> Choose a nickname", function () {
 
   it("should be completed", async function () {
     // according the challenge task, we know address of the Capture The Ether contract
-    const mainAddress = await main.getAddress();
-    const mainArtifact = await hre.artifacts.readArtifact("CaptureTheEther");
-    const mainInstance = new BaseContract("CaptureTheEther", mainArtifact.abi, player).attach(
-      mainAddress,
-    ) as CaptureTheEther;
+
+    const mainInstance = CaptureTheEther__factory.connect(await main.getAddress(), player);
 
     // deploy Nickname level through CaptureTheEther main contract
     const tx = await mainInstance.deployNicknameChallenge(player);
@@ -35,17 +31,15 @@ describe("2. Warmup -> Choose a nickname", function () {
     // set player's nickname through CaptureTheEther main contract
     const tx1 = await mainInstance.setNickname(hre.ethers.encodeBytes32String("PurrProof"));
     await expect(tx1).to.be.not.reverted;
-    await tx1.wait();
 
     // check nickname is set
-    expect(hre.ethers.decodeBytes32String(await mainInstance.nicknameOf(player))).to.be.eq("PurrProof");
+    expect(hre.ethers.decodeBytes32String(await main.nicknameOf(player))).to.be.eq("PurrProof");
 
     // check level completeness
-    const levelAddress = await main.mapPlayerNicknameChallenge(player);
-    const levelArtifact = await hre.artifacts.readArtifact("NicknameChallenge");
-    const levelInstance = new BaseContract("NicknameChallenge", levelArtifact.abi, player).attach(
-      levelAddress,
-    ) as NicknameChallenge;
-    expect(await levelInstance.connect(player).isComplete()).to.be.true;
+    const levelInstance: NicknameChallenge = NicknameChallenge__factory.connect(
+      await main.mapPlayerNicknameChallenge(player),
+      player,
+    );
+    expect(await levelInstance.isComplete()).to.be.true;
   });
 });
---
+++ test/03-guess-the-number.ts
@@ -0,0 +1,29 @@
+import { SignerWithAddress as HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+import { expect } from "chai";
+import hre from "hardhat";
+
+import { GuessTheNumberChallenge, GuessTheNumberChallenge__factory } from "../typechain-types";
+
+describe("3. Lotteries -> Guess the number  ", function () {
+  let owner: HardhatEthersSigner;
+  let player: HardhatEthersSigner;
+  let level: GuessTheNumberChallenge;
+
+  beforeEach(async function () {
+    [owner, player] = await hre.ethers.getSigners();
+    player;
+
+    const factory: GuessTheNumberChallenge__factory = new GuessTheNumberChallenge__factory(owner);
+    level = await factory.deploy({ value: hre.ethers.parseEther("1") });
+    await level.waitForDeployment();
+  });
+
+  it("should be completed", async function () {
+    // number is hardcoded in contract
+    const tx = await level.guess(42n, { value: hre.ethers.parseEther("1") });
+    await expect(tx).to.be.not.reverted;
+    await tx.wait();
+
+    expect(await level.isComplete()).to.be.true;
+  });
+});
